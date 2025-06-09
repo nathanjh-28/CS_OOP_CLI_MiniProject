217679e4787ad63341dac4692064dc0655293df8
nathanjh-28 <nathan@nathanjharris.com>
2025-06-09 11:47:06 -0400

    tweaked print statements and fixed warning

--- Projects/Program.cs
+++ Projects/Program.cs
@@ -9,18 +9,22 @@
 
 using Projects;
 
+Console.WriteLine("");
 Console.WriteLine("Hello and welcome to the Projects CLI App.");
+Console.WriteLine("");
 
 var my_proj = new Project("Steve the Client", 2);
+Console.WriteLine("Adding tasks: sign agreement, scout location, approve short list");
 my_proj.AddTask("sign agreement", 1);
 my_proj.AddTask("scout location", 2);
 my_proj.AddTask("Approve shot list", 3);
-Console.WriteLine($"next task: {my_proj.NextUnfinishedTask.Taskname}");
-Console.WriteLine("completing first task");
+Console.WriteLine($"next task: {my_proj.NextUnfinishedTask?.Taskname}");
+Console.WriteLine("***completing task***");
 my_proj.CompleteTask();
-Console.WriteLine($"next task: {my_proj.NextUnfinishedTask.Taskname}");
+Console.WriteLine($"next task: {my_proj.NextUnfinishedTask?.Taskname}");
+Console.WriteLine("");
+Console.WriteLine("Task List:");
 Console.WriteLine(my_proj.GetTaskList());
 
-Console.WriteLine("Demo complete!");
+Console.WriteLine("Demo of Projects CLI App complete!");
 
-// demonstrate app
